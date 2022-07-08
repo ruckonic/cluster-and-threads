eac0f5b4304f942d842e99eeca907509fea7bab0
ruckonic <ruckonic@icloud.com>
2022-07-08 13:58:09 -0600

    process text

--- src/server.js
+++ src/server.js
@@ -1,22 +1,31 @@
+import fs from 'node:fs'
 import http from 'node:http'
+import { URL } from 'node:url'
 
 const PORT = process.env.PORT
 
 const routes = {
   /**
    * Print a hello world
-   * @param {http.IncomingMessage} req 
-   * @param {http.ServerResponse} res 
+   * @param {http.IncomingMessage} req
+   * @param {http.ServerResponse} res
    */
   GET(req, res) {
     res.writeHead(200)
-    res.write('Hello world')
+    const url = new URL('../example.txt', import.meta.url)
+    const data = fs.readFileSync(url, {
+      encoding: 'utf8',
+    })
+
+    const str = data.repeat(10).replace(/f/g, 'sds').toLowerCase()
+
+    res.write(str)
     res.end()
-  }
+  },
 }
 
 const server = http.createServer((req, res) => {
-  process.stdout.write(`[${req.method}] ${req.url}`)
+  process.stdout.write(`[${req.method}] ${req.url}\n`)
   if (req.url === '/') {
     return routes[req.method](req, res)
   }
