43f6ce22f05ba5073615975d11dbbcba87325498
ruckonic <ruckonic@icloud.com>
2022-07-16 08:43:55 -0600

    fix: delete appsignal

--- appsignal.cjs
+++
@@ -1,9 +0,0 @@
-const { Appsignal } = require('@appsignal/nodejs')
-
-const appsignal = new Appsignal({
-  active: true,
-  name: 'Test',
-  pushApiKey: '005b9e64-569e-4a18-af1d-78cb8254cdf9',
-})
-
-module.exports = { appsignal }
